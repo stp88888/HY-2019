447eb5b0dec552566787013bf50463a91d70d080
stp88888 <stp88888@users.noreply.github.com>
2019-08-08 18:16:12 +0800

    update

--- 安泰杯-跨境电商智能算法大赛/gnn/run_session-gnn.py
+++ 安泰杯-跨境电商智能算法大赛/gnn/run_session-gnn.py
@@ -53,22 +53,24 @@ if inital:
     #     .sort_values('irank')[['buyer_admin_id', 'item_id']]
     #     .groupby('buyer_admin_id'))
     # tra = {i['buyer_admin_id'].iloc[0]: list(i['item_id']) for _, i in tra}
-    # tes = (data_test[['buyer_admin_id', 'item_id', 'irank']]
-    #     .sort_values('irank')[['buyer_admin_id', 'item_id']]
-    #     .groupby('buyer_admin_id'))
-    # tes = {i['buyer_admin_id'].iloc[0]: list(i['item_id']) for _, i in tes}
+    tes = (data_test[['buyer_admin_id', 'item_id', 'irank']]
+        .sort_values('irank')[['buyer_admin_id', 'item_id']]
+        .groupby('buyer_admin_id'))
+    tes = {i['buyer_admin_id'].iloc[0]: list(i['item_id']) for _, i in tes}
     data = (data_all[['buyer_admin_id', 'item_id', 'irank']]
         .sort_values('irank')[['buyer_admin_id', 'item_id']]
         .groupby('buyer_admin_id'))
     data = {i['buyer_admin_id'].iloc[0]: list(i['item_id']) for _, i in data}
 
     # pickle.dump(tra, open('tra.pkl', 'wb'))
-    # pickle.dump(tes, open('tes.pkl', 'wb'))
+    pickle.dump(tes, open('tes.pkl', 'wb'))
     pickle.dump(data, open('data.pkl', 'wb'))
+
+    del data_all, data_train, data_test
 else:
     print ('loading previous data.')
     #tra = pickle.load(open('tra.pkl', 'rb'))
-    #tes = pickle.load(open('tes.pkl', 'rb'))
+    tes = pickle.load(open('tes.pkl', 'rb'))
     data = pickle.load(open('data.pkl', 'rb'))
     item_user = pickle.load(open('item_user.pkl', 'rb'))
 
@@ -97,8 +99,8 @@ def TransformDataToSeq(df_dict, item_user):
                 item_dict[i] = ctr
                 output += [ctr]
                 ctr += 1
-            if len(output) < 2:
-                continue
+        if len(output) < 2:
+            continue
         ids += [sess_id]
         seqs += [output]
     print (ctr)
@@ -131,107 +133,25 @@ joblib.dump(np.array(data_label), open('train_label', 'wb'), compress=3)
 joblib.dump(item_correspond_dict, open('item_corr_dict', 'wb'), compress=3)
 
 time2 = time.time()
-print ('data preprocessing complete, time cost:', time2-time1)
-
-'''
-###
-print ('start model training.')
-def data_masks(all_usr_pois, item_tail):
-    us_lens = [len(upois) for upois in all_usr_pois]
-    len_max = max(us_lens)
-    us_pois = [upois + item_tail * (len_max - le) for upois, le in zip(all_usr_pois, us_lens)]
-    us_msks = [[1] * le + [0] * (len_max - le) for le in us_lens]
-    return us_pois, us_msks, len_max
+print ('train data preprocessing complete, time cost:', time2-time1)
 
 
-def split_validation(train_set, valid_portion):
-    train_set_x, train_set_y = train_set
-    n_samples = len(train_set_x)
-    sidx = np.arange(n_samples, dtype='int32')
-    np.random.shuffle(sidx)
-    n_train = int(np.round(n_samples * (1. - valid_portion)))
-    valid_set_x = [train_set_x[s] for s in sidx[n_train:]]
-    valid_set_y = [train_set_y[s] for s in sidx[n_train:]]
-    train_set_x = [train_set_x[s] for s in sidx[:n_train]]
-    train_set_y = [train_set_y[s] for s in sidx[:n_train]]
+def TransformDataToSeq_Predict(df_dict, item_user, item_dict):
+    ids = []
+    seqs = []
+    for sess_id, seq in df_dict.items():
+        output = []
+        for i in seq:
+            if i in item_dict:
+                output += [item_dict[i]]
+        if len(output) < 2:
+            continue
+        ids += [sess_id]
+        seqs += [output]
+    return ids, seqs
 
-    return (train_set_x, train_set_y), (valid_set_x, valid_set_y)
+test_ids, test_seq = TransformDataToSeq_Predict(tes, item_user, item_correspond_dict)
+joblib.dump(np.array(test_seq), open('test_seq', 'wb'), compress=3)
 
-
-class Data():
-    def __init__(self, data, sub_graph=False, method='ggnn', sparse=False, shuffle=False):
-        inputs = data[0]
-        inputs, mask, len_max = data_masks(inputs, [0])
-        self.inputs = np.asarray(inputs)
-        self.mask = np.asarray(mask)
-        self.len_max = len_max
-        self.targets = np.asarray(data[1])
-        self.length = len(inputs)
-        self.shuffle = shuffle
-        self.sub_graph = sub_graph
-        self.sparse = sparse
-        self.method = method
-
-    def generate_batch(self, batch_size):
-        if self.shuffle:
-            shuffled_arg = np.arange(self.length)
-            np.random.shuffle(shuffled_arg)
-            self.inputs = self.inputs[shuffled_arg]
-            self.mask = self.mask[shuffled_arg]
-            self.targets = self.targets[shuffled_arg]
-        n_batch = int(self.length / batch_size)
-        if self.length % batch_size != 0:
-            n_batch += 1
-        slices = np.split(np.arange(n_batch * batch_size), n_batch)
-        slices[-1] = np.arange(self.length-batch_size, self.length)
-        return slices
-
-    def get_slice(self, index):
-        if 1:
-            items, n_node, A_in, A_out, alias_inputs = [], [], [], [], []
-            for u_input in self.inputs[index]:
-                n_node.append(len(np.unique(u_input)))
-            max_n_node = np.max(n_node)
-            if self.method == 'ggnn':
-                for u_input in self.inputs[index]:
-                    node = np.unique(u_input)
-                    items.append(node.tolist() + (max_n_node - len(node)) * [0])
-                    u_A = np.zeros((max_n_node, max_n_node))
-                    for i in np.arange(len(u_input) - 1):
-                        if u_input[i + 1] == 0:
-                            break
-                        u = np.where(node == u_input[i])[0][0]
-                        v = np.where(node == u_input[i + 1])[0][0]
-                        u_A[u][v] = 1
-                    u_sum_in = np.sum(u_A, 0)
-                    u_sum_in[np.where(u_sum_in == 0)] = 1
-                    u_A_in = np.divide(u_A, u_sum_in)
-                    u_sum_out = np.sum(u_A, 1)
-                    u_sum_out[np.where(u_sum_out == 0)] = 1
-                    u_A_out = np.divide(u_A.transpose(), u_sum_out)
-
-                    A_in.append(u_A_in)
-                    A_out.append(u_A_out)
-                    alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
-                return A_in, A_out, alias_inputs, items, self.mask[index], self.targets[index]
-            elif self.method == 'gat':
-                A_in = []
-                A_out = []
-                for u_input in self.inputs[index]:
-                    node = np.unique(u_input)
-                    items.append(node.tolist() + (max_n_node - len(node)) * [0])
-                    u_A = np.eye(max_n_node)
-                    for i in np.arange(len(u_input) - 1):
-                        if u_input[i + 1] == 0:
-                            break
-                        u = np.where(node == u_input[i])[0][0]
-                        v = np.where(node == u_input[i + 1])[0][0]
-                        u_A[u][v] = 1
-                    A_in.append(-1e9 * (1 - u_A))
-                    A_out.append(-1e9 * (1 - u_A.transpose()))
-                    alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
-                return A_in, A_out, alias_inputs, items, self.mask[index], self.targets[index]
-
-        else:
-            return self.inputs[index], self.mask[index], self.targets[index]
-'''
+time3 = time.time()
+print ('test data preprocessing complete, time cost:', time3-time2)
---
+++ 安泰杯-跨境电商智能算法大赛/gnn/run_sr-gnn_model.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 14:46:52 2019
+
+@author: sutianpei
+"""
+
+from __future__ import division
+import numpy as np
+import joblib
+import argparse
+import datetime
+import tensorflow as tf
+import math
+
+
+print ('start model training.')
+def data_masks(all_usr_pois, item_tail):
+    us_lens = [len(upois) for upois in all_usr_pois]
+    len_max = max(us_lens)
+    us_pois = [upois + item_tail * (len_max - le) for upois, le in zip(all_usr_pois, us_lens)]
+    us_msks = [[1] * le + [0] * (len_max - le) for le in us_lens]
+    return us_pois, us_msks, len_max
+
+
+def split_validation(train_set, valid_portion):
+    train_set_x, train_set_y = train_set
+    n_samples = len(train_set_x)
+    sidx = np.arange(n_samples, dtype='int32')
+    np.random.shuffle(sidx)
+    n_train = int(np.round(n_samples * (1. - valid_portion)))
+    valid_set_x = [train_set_x[s] for s in sidx[n_train:]]
+    valid_set_y = [train_set_y[s] for s in sidx[n_train:]]
+    train_set_x = [train_set_x[s] for s in sidx[:n_train]]
+    train_set_y = [train_set_y[s] for s in sidx[:n_train]]
+
+    return (train_set_x, train_set_y), (valid_set_x, valid_set_y)
+
+
+class Data():
+    def __init__(self, data, sub_graph=False, sparse=False, shuffle=False):
+        inputs = data[0]
+        inputs, mask, len_max = data_masks(inputs, [0])
+        self.inputs = np.asarray(inputs)
+        self.mask = np.asarray(mask)
+        self.len_max = len_max
+        self.targets = np.asarray(data[1])
+        self.length = len(inputs)
+        self.shuffle = shuffle
+        self.sub_graph = sub_graph
+        self.sparse = sparse
+
+    def generate_batch(self, batch_size):
+        if self.shuffle:
+            shuffled_arg = np.arange(self.length)
+            np.random.shuffle(shuffled_arg)
+            self.inputs = self.inputs[shuffled_arg]
+            self.mask = self.mask[shuffled_arg]
+            self.targets = self.targets[shuffled_arg]
+        n_batch = int(self.length / batch_size)
+        if self.length % batch_size != 0:
+            n_batch += 1
+        slices = np.split(np.arange(n_batch * batch_size), n_batch)
+        slices[-1] = np.arange(self.length-batch_size, self.length)
+        return slices
+
+    def get_slice(self, index):
+        if 1:
+            items, n_node, A_in, A_out, alias_inputs = [], [], [], [], []
+            for u_input in self.inputs[index]:
+                n_node.append(len(np.unique(u_input)))
+            max_n_node = np.max(n_node)
+            for u_input in self.inputs[index]:
+                node = np.unique(u_input)
+                items.append(node.tolist() + (max_n_node - len(node)) * [0])
+                u_A = np.zeros((max_n_node, max_n_node))
+                for i in np.arange(len(u_input) - 1):
+                    if u_input[i + 1] == 0:
+                        break
+                    u = np.where(node == u_input[i])[0][0]
+                    v = np.where(node == u_input[i + 1])[0][0]
+                    u_A[u][v] = 1
+                u_sum_in = np.sum(u_A, 0)
+                u_sum_in[np.where(u_sum_in == 0)] = 1
+                u_A_in = np.divide(u_A, u_sum_in)
+                u_sum_out = np.sum(u_A, 1)
+                u_sum_out[np.where(u_sum_out == 0)] = 1
+                u_A_out = np.divide(u_A.transpose(), u_sum_out)
+
+                A_in.append(u_A_in)
+                A_out.append(u_A_out)
+                alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
+            return A_in, A_out, alias_inputs, items, self.mask[index], self.targets[index]
+        else:
+            return self.inputs[index], self.mask[index], self.targets[index]
+
+
+class Model(object):
+    def __init__(self, hidden_size=100, out_size=100, batch_size=100, nonhybrid=True):
+        self.hidden_size = hidden_size
+        self.out_size = out_size
+        self.batch_size = batch_size
+        self.mask = tf.placeholder(dtype=tf.float32)
+        self.alias = tf.placeholder(dtype=tf.int32)  # 给给每个输入重新
+        self.item = tf.placeholder(dtype=tf.int32)   # 重新编号的序列构成的矩阵
+        self.tar = tf.placeholder(dtype=tf.int32)
+        self.nonhybrid = nonhybrid
+        self.stdv = 1.0 / math.sqrt(self.hidden_size)
+
+        self.nasr_w1 = tf.get_variable('nasr_w1', [self.out_size, self.out_size], dtype=tf.float32,
+                                       initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.nasr_w2 = tf.get_variable('nasr_w2', [self.out_size, self.out_size], dtype=tf.float32,
+                                       initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.nasr_v = tf.get_variable('nasrv', [1, self.out_size], dtype=tf.float32,
+                                      initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.nasr_b = tf.get_variable('nasr_b', [self.out_size], dtype=tf.float32, initializer=tf.zeros_initializer())
+
+    def forward(self, re_embedding, train=True):
+        rm = tf.reduce_sum(self.mask, 1)
+        last_id = tf.gather_nd(self.alias, tf.stack([tf.range(self.batch_size), tf.to_int32(rm)-1], axis=1))
+        last_h = tf.gather_nd(re_embedding, tf.stack([tf.range(self.batch_size), last_id], axis=1))
+        seq_h = tf.stack([tf.nn.embedding_lookup(re_embedding[i], self.alias[i]) for i in range(self.batch_size)],
+                         axis=0)                                                           #batch_size*T*d
+        last = tf.matmul(last_h, self.nasr_w1)
+        seq = tf.matmul(tf.reshape(seq_h, [-1, self.out_size]), self.nasr_w2)
+        last = tf.reshape(last, [self.batch_size, 1, -1])
+        m = tf.nn.sigmoid(last + tf.reshape(seq, [self.batch_size, -1, self.out_size]) + self.nasr_b)
+        coef = tf.matmul(tf.reshape(m, [-1, self.out_size]), self.nasr_v, transpose_b=True) * tf.reshape(
+            self.mask, [-1, 1])
+        b = self.embedding[1:]
+        if not self.nonhybrid:
+            ma = tf.concat([tf.reduce_sum(tf.reshape(coef, [self.batch_size, -1, 1]) * seq_h, 1),
+                            tf.reshape(last, [-1, self.out_size])], -1)
+            self.B = tf.get_variable('B', [2 * self.out_size, self.out_size],
+                                     initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+            y1 = tf.matmul(ma, self.B)
+            logits = tf.matmul(y1, b, transpose_b=True)
+        else:
+            ma = tf.reduce_sum(tf.reshape(coef, [self.batch_size, -1, 1]) * seq_h, 1)
+            logits = tf.matmul(ma, b, transpose_b=True)
+        loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.tar - 1, logits=logits))
+        self.vars = tf.trainable_variables()
+        if train:
+            lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in self.vars if v.name not
+                               in ['bias', 'gamma', 'b', 'g', 'beta']]) * self.L2
+            loss = loss + lossL2
+        return loss, logits
+
+    def run(self, fetches, tar, item, adj_in, adj_out, alias, mask):
+        return self.sess.run(fetches, feed_dict={self.tar: tar, self.item: item, self.adj_in: adj_in,
+                                                 self.adj_out: adj_out, self.alias: alias, self.mask: mask})
+
+
+class GGNN(Model):
+    def __init__(self,hidden_size=100, out_size=100, batch_size=300, n_node=None,
+                 lr=None, l2=None, step=1, decay=None, lr_dc=0.1, nonhybrid=False):
+        super(GGNN,self).__init__(hidden_size, out_size, batch_size, nonhybrid)
+        self.embedding = tf.get_variable(shape=[n_node, hidden_size], name='embedding', dtype=tf.float32,
+                                         initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.adj_in = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, None, None])
+        self.adj_out = tf.placeholder(dtype=tf.float32, shape=[self.batch_size, None, None])
+        self.n_node = n_node
+        self.L2 = l2
+        self.step = step
+        self.nonhybrid = nonhybrid
+        self.W_in = tf.get_variable('W_in', shape=[self.out_size, self.out_size], dtype=tf.float32,
+                                    initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.b_in = tf.get_variable('b_in', [self.out_size], dtype=tf.float32,
+                                    initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.W_out = tf.get_variable('W_out', [self.out_size, self.out_size], dtype=tf.float32,
+                                     initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        self.b_out = tf.get_variable('b_out', [self.out_size], dtype=tf.float32,
+                                     initializer=tf.random_uniform_initializer(-self.stdv, self.stdv))
+        with tf.variable_scope('ggnn_model', reuse=None):
+            self.loss_train, _ = self.forward(self.ggnn())
+        with tf.variable_scope('ggnn_model', reuse=True):
+            self.loss_test, self.score_test = self.forward(self.ggnn(), train=False)
+        self.global_step = tf.Variable(0)
+        self.learning_rate = tf.train.exponential_decay(lr, global_step=self.global_step, decay_steps=decay,
+                                                        decay_rate=lr_dc, staircase=True)
+        self.opt = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss_train, global_step=self.global_step)
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+        config = tf.ConfigProto(gpu_options=gpu_options)
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+        self.sess.run(tf.global_variables_initializer())
+
+    def ggnn(self):
+        fin_state = tf.nn.embedding_lookup(self.embedding, self.item)
+        cell = tf.nn.rnn_cell.GRUCell(self.out_size)
+        with tf.variable_scope('gru'):
+            for i in range(self.step):
+                fin_state = tf.reshape(fin_state, [self.batch_size, -1, self.out_size])
+                fin_state_in = tf.reshape(tf.matmul(tf.reshape(fin_state, [-1, self.out_size]),
+                                                    self.W_in) + self.b_in, [self.batch_size, -1, self.out_size])
+                fin_state_out = tf.reshape(tf.matmul(tf.reshape(fin_state, [-1, self.out_size]),
+                                                     self.W_out) + self.b_out, [self.batch_size, -1, self.out_size])
+                av = tf.concat([tf.matmul(self.adj_in, fin_state_in),
+                                tf.matmul(self.adj_out, fin_state_out)], axis=-1)
+                state_output, fin_state = \
+                    tf.nn.dynamic_rnn(cell, tf.expand_dims(tf.reshape(av, [-1, 2*self.out_size]), axis=1),
+                                      initial_state=tf.reshape(fin_state, [-1, self.out_size]))
+        return tf.reshape(fin_state, [self.batch_size, -1, self.out_size])
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default='sample', help='dataset name: diginetica/yoochoose1_4/yoochoose1_64/sample')
+parser.add_argument('--validation', action='store_true', help='validation')
+parser.add_argument('--epoch', type=int, default=30, help='number of epochs to train for')
+parser.add_argument('--batchSize', type=int, default=100, help='input batch size')
+parser.add_argument('--hiddenSize', type=int, default=100, help='hidden state size')
+parser.add_argument('--l2', type=float, default=1e-5, help='l2 penalty')
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--step', type=int, default=1, help='gnn propogation steps')
+parser.add_argument('--nonhybrid', action='store_true', help='global preference')
+parser.add_argument('--lr_dc', type=float, default=0.1, help='learning rate decay rate')
+parser.add_argument('--lr_dc_step', type=int, default=3, help='the number of steps after which the learning rate decay')
+opt = parser.parse_args()
+
+para = {}
+para['validation'] = 'store_true'
+para['epoch'] = 30
+para['batchSize'] = 100
+para['hiddenSize'] = 100
+para['validation'] = 'store_true'
+para['l2'] = 1e-5
+para['lr'] = 0.001
+para['step'] = 1
+para['nonhybrid'] = 'store_true'
+para['lr_dc'] = 0.1
+para['lr_dc_step'] = 3
+n_node = np.loadtxt('sr-gnn_tmp.txt', dtype=int)
+
+train_label = joblib.load(open('train_label', 'rb'))
+data_seq = joblib.load(open('data_seq', 'rb'))
+item_corr_dict = joblib.load(open('item_corr_dict', 'rb'))
+
+train_data = Data((data_seq, train_label), sub_graph=True, shuffle=True)
+model = GGNN(hidden_size=para['hiddenSize'], out_size=para['hiddenSize'], 
+             batch_size=para['batchSize'], n_node=n_node, lr=para['lr'], 
+             l2=para['l2'],  step=para['step'], 
+             decay=para['lr_dc_step'] * len(train_data.inputs) / para['batchSize'],
+             lr_dc=para['lr_dc'], nonhybrid=opt.nonhybrid)
